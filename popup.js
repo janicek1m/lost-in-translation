63cddb9316041c3fa06a711a778cfdbb47840eff
Joanne Chen <33409024+joannejchen@users.noreply.github.com>
2020-07-28 17:26:47 -0500

    update pop-up button for easy mode
    changes color of button when clicked

--- popup.js
+++ popup.js
@@ -4,6 +4,9 @@ function runEasyMode() {
     chrome.tabs.executeScript({
         file: 'content.js'
     });
+    var button = document.getElementById('easyMode');
+    button.setAttribute("disabled", "true");
+    button.style.backgroundColor = "#A3EA67";
 }
 
 document.getElementById('easyMode').addEventListener('click', runEasyMode);
