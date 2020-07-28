c25b098ff31d053b3608c02b49eb2ad32f4d2453
Joanne Chen <33409024+joannejchen@users.noreply.github.com>
2020-07-28 17:54:31 -0500

    update content.js to change multiple words

--- content.js
+++ content.js
@@ -1,23 +1,22 @@
 var elements = document.getElementsByTagName('*');
 
-let dictionary = {
-    that: "que",
-    in: "en",
-    why: "porque",
-    after: "despues"
-}
+let spanishDictionary = ["que", "en", "porque", "despues"];
+let englishDictionary = ["that", "in", "why", "after"];
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
+    for(var k = 0; k < englishDictionary.length; k++) {
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/that/gi, 'que');
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+                var searchWord = new RegExp(englishDictionary[k], "gi");
+                var replacedText = text.replace(searchWord, spanishDictionary[k]);
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
             }
         }
     }
