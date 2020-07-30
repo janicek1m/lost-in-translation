96209520281700cb0a6a47fde229f9622e0f817f
Joanne Chen <33409024+joannejchen@users.noreply.github.com>
2020-07-30 09:25:40 -0500

    changed spanishDictionary to translatedDictionary

--- popup.js
+++ popup.js
@@ -18,49 +18,27 @@ document.addEventListener('DOMContentLoaded', documentEvents, false);
 function addToDictionary(input) {
     let words = input.value.split(" ");
     if (words.length == 2) {
-        chrome.storage.sync.get(["englishDictionary", "spanishDictionary"], function(result) {
+        chrome.storage.sync.get(["englishDictionary", "translatedDictionary"], function(result) {
             result.englishDictionary.push(words[0]);
-            result.spanishDictionary.push(words[1]);
+            result.translatedDictionary.push(words[1]);
             let tempDict = result.englishDictionary;
-            let tempDict2 = result.spanishDictionary;
+            let tempDict2 = result.translatedDictionary;
             chrome.storage.sync.remove('englishDictionary', function() {
                 console.log("removed");
             });
             chrome.storage.sync.set({ 'englishDictionary': tempDict }, function() {
                 console.log('updated english dictionary');
             });
-            chrome.storage.sync.remove('spanishDictionary', function() {
+            chrome.storage.sync.remove('translatedDictionary', function() {
                 console.log("removed");
             });
-            chrome.storage.sync.set({ 'spanishDictionary': tempDict2 }, function() {
-                console.log('updated spanish dictionary');
+            chrome.storage.sync.set({ 'translatedDictionary': tempDict2 }, function() {
+                console.log('updated translated dictionary');
             });
         });
         var okButton = document.getElementById("ok_btn");
         okButton.setAttribute("disabled", "true");
         okButton.style.backgroundColor = "#C0c3BE";
-        /*
-        chrome.storage.sync.get('englishDictionary', function(result) {
-            result.englishDictionary.push(words[0]);
-            let tempDict = result.englishDictionary;
-            chrome.storage.sync.remove('englishDictionary', function() {
-                console.log("removed");
-            });
-            chrome.storage.sync.set({'englishDictionary': tempDict}, function() {
-                console.log("updated english dictionary");
-            })
-        });
-        chrome.storage.sync.get('spanishDictionary', function(result) {
-            result.spanishDictionary.push(words[0]);
-            let tempDict = result.spanishDictionary;
-            chrome.storage.sync.remove('spanishDictionary', function() {
-                console.log("removed");
-            });
-            chrome.storage.sync.set({'spanishDictionary': tempDict}, function() {
-                console.log("updated spanish dictionary");
-            })
-        });
-        */
     }
 }
 
