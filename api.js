98d887bda423dfd530a5e0a87d5352c80eef8945
Janice Kim <janicekim@MacBook-Pro.fios-router.home>
2020-07-29 23:36:00 -0400

    Updated api.js with api array test

--- api.js
+++ api.js
@@ -1,30 +1,57 @@
-var elements = document.getElementsByTagName('*');
+var theAPIKey = "AIzaSyBttL3_rUfMaP8vZQazT8bCd5XhHkmR4lA";
+var array = ["hi", "hello", "goodbye", "I am sad"];
+var translatedArray = [];
 
-// let english = ["that", "in", "why", "after"];
-// let spanish = ["que", "en", "porque", "despues"];
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+// function translateArray(/x){
+for (var i = 0; i < array.length; i++) {
+    var temp = array[i].toString();
+    translation(temp);
+}
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            // for (var k = 0; k < english.length; k++) {
-            var replacedText = text.replace(/that/gi, 'que');
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-                // }
-                // replaceText(english[k], spanish[k]);
-            }
-        }
+function translation(word) {
+    var baseUrl = "https://translation.googleapis.com/language/translate/v2"
+    var content = {
+        'q': word,
+        //es = spanish, fr = french, etc.
+        'target': "fr"
+    };
+
+    ajaxRequest("POST", baseUrl + "?key=" + theAPIKey, handleTranslationResponse, JSON.stringify(content));
+}
+
+
+function handleTranslationResponse() {
+    /**/
+    if (successfulRequest(this)) {
+        /*This is where you got the response with the "OK" status so you can go ahead and parse this.responseText*/
+        var response_json = JSON.parse(this.responseText);
+        var translated_text = "";
+        response_json["data"]["translations"].forEach(function(element) {
+            translated_text += element['translatedText'] + '\n';
+        });
+        //fr = french, es = english , etc.
+        var target_lang = "fr";
+        translatedArray.push(translated_text.toString());
+        // console.log(translatedArray);
+    }
+    console.log(translatedArray);
+}
+
+
+function ajaxRequest(method, url, handlerFunction, content) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open(method, url);
+    xhttp.onreadystatechange = handlerFunction;
+    if (method == "POST") {
+        xhttp.send(content);
+    } else {
+        xhttp.send();
     }
 }
 
-// function replaceText(english, spanish) {
-//     var replacedText = text.replace(english gi, `${spanish}`);
-//     if (replacedText !== text) {
-//         element.replaceChild(document.createTextNode(replacedText), node);
-//     }
-// }
+/*Helper function: checks if the response to the request is ready to process*/
+function successfulRequest(request) {
+    return request.readyState === 4 && request.status == 200;
+}
