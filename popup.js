f2dfaecbab774b2556e252f0d4f5ab250fe5b8ad
Joanne Chen <33409024+joannejchen@users.noreply.github.com>
2020-07-30 14:25:46 -0500

    uses api to translate user-inputted words

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let easyMode = document.getElementById('easyMode');
+var theAPIKey = "AIzaSyBttL3_rUfMaP8vZQazT8bCd5XhHkmR4lA";
 
 function runEasyMode() {
     chrome.tabs.executeScript({
@@ -15,31 +16,84 @@ document.getElementById('easyMode').addEventListener('click', runEasyMode);
 
 document.addEventListener('DOMContentLoaded', documentEvents, false);
 
+var translatedWord = "";
+
 function addToDictionary(input) {
-    let words = input.value.split(" ");
-    if (words.length == 2) {
-        chrome.storage.sync.get(["englishDictionary", "translatedDictionary"], function(result) {
-            result.englishDictionary.push(words[0]);
-            result.translatedDictionary.push(words[1]);
-            let tempDict = result.englishDictionary;
-            let tempDict2 = result.translatedDictionary;
-            chrome.storage.sync.remove('englishDictionary', function() {
-                console.log("removed");
-            });
-            chrome.storage.sync.set({ 'englishDictionary': tempDict }, function() {
-                console.log('updated english dictionary');
-            });
-            chrome.storage.sync.remove('translatedDictionary', function() {
-                console.log("removed");
-            });
-            chrome.storage.sync.set({ 'translatedDictionary': tempDict2 }, function() {
-                console.log('updated translated dictionary');
-            });
+    let englishWord = input.value;
+    chrome.storage.sync.get(["englishDictionary", "translatedDictionary"], function(result) {
+        result.englishDictionary.push(englishWord);
+        let tempEnglishDictionary = result.englishDictionary;
+        let tempTranslatedDictionary = result.translatedDictionary;
+        translation(englishWord, tempTranslatedDictionary.length);
+        function translation(word, num) {
+            var baseUrl = "https://translation.googleapis.com/language/translate/v2"
+            var content = {
+                'q': word + " " + num,
+                //es = spanish, fr = french, etc.
+                'target': "es"
+            };
+        
+            ajaxRequest("POST", baseUrl + "?key=" + theAPIKey, handleTranslationResponse, JSON.stringify(content));
+        }
+        
+        
+        function handleTranslationResponse() {
+            /**/
+            if (successfulRequest(this)) {
+                var response_json = JSON.parse(this.responseText);
+                var tempText = response_json["data"]["translations"][0].translatedText;
+                /*This is where you got the response with the "OK" status so you can go ahead and parse this.responseText*/
+                var words = tempText.split(" ");
+                console.log(words);
+                tempTranslatedDictionary[+words[1]] = words[0];
+                console.log(tempTranslatedDictionary);
+                //console.log(tempTranslatedDictionary);
+                chrome.storage.sync.remove('translatedDictionary', function() {
+                    console.log("removed");
+                });
+                chrome.storage.sync.set({ 'translatedDictionary': tempTranslatedDictionary }, function() {
+                    console.log(tempTranslatedDictionary);
+                    console.log('updated translated dictionary');
+                });
+            }
+        
+        }
+        // console.log(translatedArray);
+        
+        function ajaxRequest(method, url, handlerFunction, content) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.open(method, url);
+            xhttp.onreadystatechange = handlerFunction;
+            if (method == "POST") {
+                xhttp.send(content);
+            } else {
+                xhttp.send();
+            }
+        }
+        
+        /*Helper function: checks if the response to the request is ready to process*/
+        function successfulRequest(request) {
+            return request.readyState === 4 && request.status == 200;
+        }
+
+        chrome.storage.sync.remove('englishDictionary', function() {
+            console.log("removed");
         });
-        var okButton = document.getElementById("ok_btn");
-        okButton.setAttribute("disabled", "true");
-        okButton.style.backgroundColor = "#C0c3BE";
-    }
+        chrome.storage.sync.set({ 'englishDictionary': tempEnglishDictionary }, function() {
+            console.log('updated english dictionary');
+        });
+        /*
+        chrome.storage.sync.remove('translatedDictionary', function() {
+            console.log("removed");
+        });
+        chrome.storage.sync.set({ 'translatedDictionary': tempDict2 }, function() {
+            console.log('updated translated dictionary');
+        });
+        */
+    });
+    var okButton = document.getElementById("ok_btn");
+    okButton.setAttribute("disabled", "true");
+    okButton.style.backgroundColor = "#C0c3BE";
 }
 
 function documentEvents() {
